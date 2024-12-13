77cbe20a2424f8ce08e48a1e59c8f9d7f499259f
Rahul Kumar Jaiswal <rahulkumar8533205@gmail.com>
2024-12-13 20:16:29 +0530

    Overloading Method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,33 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
 
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        // Overloading method
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        // Overloading method
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        System.out.println(p.product(10,20));
+
+        // calling overloaded method
+        System.out.println(p.product(10,20,30));
+
+        System.out.println(p.product(10.20,20.65));
+    }
+    }
 }
